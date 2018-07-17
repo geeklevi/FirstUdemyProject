4e1bc162adc6f6dc8672a01ea6e69a2c520f448f
Levi Hu <LEVI@Lihuas-MacBook-Pro.local>
2018-07-17 00:55:03 -0400

    before sleep

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on("ready", ()=>{
     console.log('App is now ready');
-    new BrowserWindow({});
+    const mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'main.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+});
+
+ipcMain.on('video:submit', (event, path)=>{
+    ffmpeg.ffprobe(path, (err, metadata) =>{
+        console.log('Video duration is: ', metadata.format.duration);
+        mainWindow.close();
+    });
 });
